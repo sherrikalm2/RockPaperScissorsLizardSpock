09fd53fd7c5dc02249047043ca11b73bbd715be1
skipdigit <datamama@yahoo.com>
2022-01-05 14:23:03 -0600

    changed text output to show player 1 and player 2

--- rockPaperScissorsLizardSpock.py
+++ rockPaperScissorsLizardSpock.py
@@ -31,12 +31,12 @@ b = random.choice(list(vals.keys()))
 #a = input('Player1 enter Rock, Paper, Scissors, Lizard or Spock: ')
 #b = input('Player2 enter Rock, Paper, Scissors, Lizard or Spock: ')
 
-print('User selected: ' + a )
-print('Computer selected: ' + b)
+print('Player 1 selected: ' + a )
+print('Player 2 selected: ' + b)
 
 if b in vals[a]:
-    print('User wins! ' + a + ' ' + vals[a][b] +  ' ' + b)
+    print('Player 1 wins! ' + a + ' ' + vals[a][b] +  ' ' + b)
 elif a in vals[b]:
-    print('Computer wins! ' + b + ' ' + vals[b][a] + ' ' + a)
+    print('Player 2 wins! ' + b + ' ' + vals[b][a] + ' ' + a)
 else:
     print('Try Again')
